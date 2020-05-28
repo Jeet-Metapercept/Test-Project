f4e02cac6ef4d10a77d25d8c69c7aa7ac40f3de7
Jeet <Jeet@Metapercept.com>
2020-05-29 01:37:19 +0530

    Test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React, { Fragment } from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App.test.js";
-import * as serviceWorker from "./serviceWorker";
+//import React, { Fragment } from "react";
+//import ReactDOM from "react-dom";
+//import "./index.css";
+//import App from "./App.test.js";
+//import * as serviceWorker from "./serviceWorker";
 
 // ========================================
 
-ReactDOM.render(<App />, document.getElementById("root"));
-serviceWorker.unregister();
+//ReactDOM.render(<App />, document.getElementById("root"));
+//serviceWorker.unregister();
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Test extends React.Component {
+  render() {
+    return <h1>My Reach App!</h1>;
+  }
+}
+
+ReactDOM.render(<Test />, document.getElementById('root'));
+
